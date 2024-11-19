f72e77158cc528307089001f03ec0e9957a572b4
niksraman <ramannikete76@gmail.com>
2024-11-19 21:55:04 +0530

    ch

--- factorial.py
+++ factorial.py
@@ -1,6 +1,7 @@
-def my_factorial(n):
-    if n == 0 or n == 1:
+def factorial(n):
+    if n == 0 or n ==1:
         return 1
-    return n * my_factorial(n - 1)
+    
+    return n*factorial(n-1)
 
-print(my_factorial(5))  # Correct function name
+print(factorial(9))
